ff62fe122d609bdb5eec7373eb769c641729deb3
Julia Kudryavtseva <juliet.kudryavtseva@gmail.com>
2016-01-19 00:19:25 +0200

    Download data API with Angular

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,6 +9,26 @@
  */
 var app = angular.module('camperNewsApp');
 
-app.controller('MainCtrl', ['$scope', function($scope) {
 
+app.filter('upVotesLength', function(){
+    return function (argument) {
+        return argument.length;
+    }
+});
+
+app.filter('formattedTime', function(){
+    return function (argument) {
+        var correctTime = parseInt(argument/1000);
+        return moment.unix(correctTime).format('LLL');
+    }
+});
+
+app.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
+    $scope.allNews = [];
+    $scope.image = "../images/loading.gif";
+
+    $http.get('http://www.freecodecamp.com/news/hot')
+        .then(function(res) {
+            $scope.allNews = res.data;
+        });
 }]);
